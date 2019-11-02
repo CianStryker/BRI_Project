0f6ca0fa62fe7a824bde709888d4221e3ba6f1fe
CianStryker <cianstryker1@gmail.com>
2019-11-02 18:21:28 -0400

    Spell check

--- BRI_Shiny/app.R
+++ BRI_Shiny/app.R
@@ -60,8 +60,8 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
         column(width = 8,
                
                tags$h2("Chinese Investment"),
-               
-               tags$p("There is a consensus among experts that with the dissolution of the USSR in 1991, the world became unipolar with the United States enjoying a near total global hegemony in both political and ecnonomic terms. The explosive economic growth of China, however, from the early 2000's to now, has challenged this concept and shifted public opinion to recognize the advent of a multi-polar world. A major goal of China during this period has been domestic economic growth, but as its economy imporved drastically, so did China's investment in foreign countries. Considering the Chinese Government's economic model, the relationship between private firms and the Communist party is closure than in most other countries. Examining China's Foreign Direct Investment, therefore gives insight into the Chinese government's overall geopolitical goals. This is especially true with the announcment of the 'One Belt, One Road project' in 2014, where Xi made it clear that China was interested in recreating the Silk Road. This ambitous infastrucutral investing project is mostly targetted towards developing regions such as West Asia, the Middle East, Sub-Saharan Africa, and South America. Many experts believe that this initiative, the Belt and Road Initiative, is actually an ambitious attempt to win political influence and capture emerging markets in vital areas neglected by the U.S. with the ultimate goal of shifting the economic center of the world from New York City to Beijing. "),
+
+               tags$p("There is a consensus among experts that with the dissolution of the USSR in 1991, the world became unipolar with the United States enjoying a near total global hegemony in both political and economic terms. The explosive economic growth of China, however, from the early 2000's to now, has challenged this concept and shifted public opinion to recognize the advent of a multi-polar world. A major goal of China during this period has been domestic economic growth, but as its economy improved drastically, so did China's investment in foreign countries. Considering the Chinese Government's economic model, the relationship between private firms and the Communist party is closure than in most other countries. Examining China's Foreign Direct Investment, therefore gives insight into the Chinese government's overall geopolitical goals. This is especially true with the announcement of the 'One Belt, One Road project' in 2014, where Xi made it clear that China was interested in recreating the Silk Road. This ambitious infrastructural investing project is mostly targeted towards developing regions such as West Asia, the Middle East, Sub-Saharan Africa, and South America. Many experts believe that this initiative, the Belt and Road Initiative, is actually an ambitious attempt to win political influence and capture emerging markets in vital areas neglected by the U.S. with the ultimate goal of shifting the economic center of the world from New York City to Beijing."),
                
                tags$br()
                
@@ -85,9 +85,9 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
                
                tags$br(),
                
-               tags$p("The American Enterprise Institute and Heritage Foundation has recorded almost every single Chinese Foreign Direct Investment since 2005 to now. The cummulative results of which can be seen in the world map above.  The important note here is to see that China has invested in almost every single country in the world, but a truly massive amount in the U.S. and a few other key states."),
-            
-               tags$p("Below, however, is the culmination of all investments China has made per year. It demonstrates the staggering increase in FDI from 2005 to now. This increase in FDI closely coorelates to China's overall GDP increase in this time frame. It is clear that as China' economy became stronger and stronger, it chose to invest more and more globally."), 
+               tags$p("The American Enterprise Institute and Heritage Foundation has recorded almost every single Chinese Foreign Direct Investment since 2005 to now. The cumulative results of which can be seen in the world map above.  The important note here is to see that China has invested in almost every single country in the world, but a truly massive amount in the U.S. and a few other key states."), 
+               
+               tags$p("Below, however, is the culmination of all investments China has made per year. It demonstrates the staggering increase in FDI from 2005 to now. This increase in FDI closely correlates to China's overall GDP increase in this time frame. It is clear, that as China' economy became stronger and stronger, it chose to invest more and more globally."), 
                
                tags$br(), 
                
@@ -108,11 +108,11 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
              
              tags$h2("Chinese FDI per Region"),
              
-             tags$p("But how does Chinese FDI differ by region. In looking at the graph below we can see cummulative Chinese FDI each year per region. With it,  the regions China chose to prioritize per year are evident. In the beginning there was little difference between regions in terms of investment, mostly due to the fact that China was not investing much globally at all. Over time, however, we can see that China began to prioritize investing within a few key regions."),
-        
+             tags$p("But how does Chinese FDI differ by region. In looking at the graph below we can see cumulative Chinese FDI each year per region. With this graph the regions China chose to prioritize per year are evident. In the beginning there was little difference between regions in terms of investment, mostly because China was not investing much globally at all. Over time, however, we can see that China began to prioritize investing within a few key regions."), 
+             
              plotOutput(outputId = "plot3", height = ("600px")),
              
-             tags$p("We see  that in general the bulk of Chinese Direct Foreign Investment went to Europe, North America, and specifically the US. To demonstrate the enormity of Chinese FDI in the U.S., the United States has been separated from North America. While Europe, East Asia, West Asia, and Sub-Saharan Africa recieved the most Chinese FDI, the U.S. by itself comes in fifth place, more than all investment made into all of South America for example. If the bulk of Chinese investment flows into Europe and North America though, does this really mean that China is winning political influence world wide as many experts believe? Where does the Belt and Road Initiative fit into this picture?"),
+             tags$p("We see that in general the bulk of Chinese Direct Foreign Investment went to Europe, North America, and specifically the US. To demonstrate the enormity of Chinese FDI in the U.S., the United States has been separated from North America. While Europe, East Asia, West Asia, and Sub-Saharan Africa received the most Chinese FDI, the U.S. by itself comes in fifth place, more than all investment made into all of South America for example. If the bulk of Chinese investment flows into Europe and North America though, does this really mean that China is winning political influence worldwide as many experts believe? Where does the Belt and Road Initiative fit into this picture?"), 
              
              tags$br()
              
@@ -130,7 +130,7 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
              
              tags$h2("'One Belt, One Road'"),
              
-             tags$p("The Belt and Road initiative is not immediatley apparent when we view Chinese FDI in its totality. This is because the BRI is an investment and political strategy based on a few key, typically infastructural, sectors. Namely through the BRI the Chinese government wants to invest heavily in construction (i.e. real estate within our graph), energy, metals, and transportation. These are the sectors that China believes will facillitate the creation of a New Silk Road that will tie all of Africa, the Middle East, Asia, and potentiall even South America to Beijing. In investing in these areas, often with the help of attractive soft loans, China allows these developing countries to improve their infastructures, access their resources, and improve their connection to the world economy vis-a-vi China. In return, however, they assume massive amounts of debt to China and China gains almost sole access to the emerging markets it helped develop. This economic and political dependency is what is in line with the overall geopolitical fears regarding BRI. "),
+             tags$p("The Belt and Road initiative is not immediately apparent when we view Chinese FDI in its totality. This is because the BRI is an investment and political strategy based on a few key, typically infrastructural, sectors. Namely through the BRI the Chinese government wants to invest heavily in construction (i.e. real estate within our graph), energy, metals, and transportation. These are the sectors that China believes will facilitate the creation of a New Silk Road that will tie all of Africa, the Middle East, Asia, and potential even South America to Beijing. In investing in these areas, often with the help of attractive soft loans, China allows these developing countries to improve their infrastructures, access their resources, and improve their connection to the world economy vis-a-vi China. In return, however, they assume massive amounts of debt to China and China gains almost sole access to the emerging markets it helped develop. This economic and political dependency is what is in line with the overall geopolitical fears regarding BRI."), 
              
              tags$br(), 
              
@@ -138,7 +138,7 @@ ui <- fluidPage(theme = shinytheme("sandstone"),
              
              tags$br(), 
              
-             tags$p("Of note with the Graph above is that when we only look at the key infastrucutral sectors that comprise BRI's focus, it is clear that the four target regions of West Asia, the Middle East, Sub-Saharan Africa, and South America, recieve far more funding than Europe or the U.S. This also suggests that a closure examination of these four target regions would aid in understanding the One Belt, One Road project as a whole. ")
+             tags$p("Of note with the Graph above is that when we only look at the key infrastructural sectors that comprise BRI's focus, the four target regions of West Asia, the Middle East, Sub-Saharan Africa, and South America, receive far more funding than Europe or the U.S. This also suggests that a closure examination of these four target regions would aid in understanding the One Belt, One Road project as a whole. ")
              
       ),
   
---
+++ Objects.r
@@ -0,0 +1,22 @@
+library(readr)
+
+
+
+dir_create("BRI_Shiny/r_objects")
+write_rds(plot1, path = "BRI_Shiny/r_objects/plot1.rds")
+write_rds(plot2, path = "BRI_Shiny/r_objects/plot2.rds")
+write_rds(plot3, path = "BRI_Shiny/r_objects/plot3.rds")
+write_rds(plot4, path = "BRI_Shiny/r_objects/plot4.rds")
+write_rds(plot5, path = "BRI_Shiny/r_objects/plot5.rds")
+write_rds(plot6, path = "BRI_Shiny/r_objects/plot6.rds")
+write_rds(data7, path = "BRI_Shiny/r_objects/data7.rds")
+write_rds(plot8, path = "BRI_Shiny/r_objects/plot8.rds")
+write_rds(plot9, path = "BRI_Shiny/r_objects/plot9.rds")
+write_rds(linearMod10, path = "BRI_Shiny/r_objects/linearMod10.rds")
+write_rds(data11, path = "BRI_Shiny/r_objects/data11.rds")
+write_rds(plot12, path = "BRI_Shiny/r_objects/plot12.rds")
+write_rds(plot13, path = "BRI_Shiny/r_objects/plot13.rds")
+write_rds(linearMod14, path = "BRI_Shiny/r_objects/linearMod14.rds")
+write_rds(plot15, path = "BRI_Shiny/r_objects/plot15.rds")
+write_rds(plot16, path = "BRI_Shiny/r_objects/plot16.rds")
+
